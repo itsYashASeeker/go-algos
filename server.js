14d42a7e9fa5e3716f4d9a4e950f444aa3aec40e
itsYashASeeker <yashkc6432@gmail.com>
2023-08-02 22:48:47 +0530

    build10

--- server.js
+++ server.js
@@ -4,10 +4,14 @@ const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', function (req, res) {
+app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(process.env.PORT || 8080, () => {
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(process.env.PORT || 8090, () => {
     console.log("Algorithm Vlab running successfully!");
 });
